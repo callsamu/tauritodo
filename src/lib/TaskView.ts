8d87bef1e3f4e01f40bb56bf3d8a39f5d973b0b4
callsamu <samuelbree@gmail.com>
2024-12-10 17:11:42 -0300

    feat: implement task view class

---
+++ src/lib/TaskView.ts
@@ -0,0 +1,44 @@
+import { Task, TaskRepository } from "./types";
+import { Map as ImmutableMap } from 'immutable';
+
+export class TaskView {
+	constructor(
+		private repository: TaskRepository,
+		private tasks: ImmutableMap<string, Task>,
+	) {}
+
+	static async fromSearch(
+		repository: TaskRepository,
+		filters?: Partial<Task>
+	): Promise<TaskView> {
+		const tasks = await repository.search(filters || {});
+		const map = new Map(tasks.map(task => [task.id, task]));
+		return new TaskView(repository, ImmutableMap(map));
+	}
+
+	iter(): IterableIterator<Task> {
+		return this.tasks.values();
+	}
+
+	async search(filters?: Partial<Task>): Promise<TaskView> {
+		return TaskView.fromSearch(this.repository, filters);
+	}
+
+	async add(title: string): Promise<TaskView> {
+		const newTask = await this.repository.create(title);
+		const tasks = this.tasks.set(newTask.id, newTask);
+		return new TaskView(this.repository, tasks);
+	}
+
+	async edit(task: Task): Promise<TaskView> {
+		const editedTask = await this.repository.edit(task);
+		const tasks = this.tasks.set(editedTask.id, editedTask);
+		return new TaskView(this.repository, tasks);
+	}
+
+	async delete(id: string): Promise<TaskView> {
+		await this.repository.remove(id);
+		const tasks = this.tasks.delete(id);
+		return new TaskView(this.repository, tasks);
+	}
+}
