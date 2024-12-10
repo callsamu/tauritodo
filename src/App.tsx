939056e1237ca2b6d426f9fe9f4e43291d7e56e8
callsamu <samuelbree@gmail.com>
2024-12-10 17:22:51 -0300

    feat: list tasks on root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./components/theme-provider";
+import { Task } from "./lib/types";
+import { InMemoryTaskRepository } from "./lib/InMemoryTaskRepository";
+import { TaskView } from "./lib/TaskView";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+	const repository = new InMemoryTaskRepository();
+	const [tasks, setTasks] = useState<TaskView>(
+		TaskView.empty(repository)
+	);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+	useEffect(() => {
+		tasks.search({}).then(setTasks);
+		return () => {};
+	}, [])
 
-  return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+	const child = [];
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+	for (const task of tasks.iter()) {
+		child.push(<li key={task.id}>{task.title}</li>);
+	}
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
-  );
+	return (
+		<ThemeProvider defaultTheme="dark">
+			<main>
+				<h1>Tauri Todo</h1>
+				<ul>
+					{child}
+				</ul>
+			</main>
+		</ThemeProvider>
+	);
 }
 
 export default App;
